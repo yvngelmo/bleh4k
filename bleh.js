97fbd664fb5dc3bb54be6474e881e3ff6798d0e3
elmo <schmerbacheric@gmail.com>
2026-05-03 16:11:43 +0200

    init loading fix

--- bleh.js
+++ bleh.js
@@ -39,6 +39,7 @@ function setup() {
     bg<1 ? lerpColor(color(255), color('#00E4C2'), bg) : lerpColor(color('#00E4C2'), color(0), bg-1),
   ];
   state = "menu";
+  pstate = "init";
   windowResized();
   initlib();
 }
@@ -195,7 +196,7 @@ function calpb() {
 }
 
 function loading() {
-  if(pstate == null) background(0);
+  if(pstate === "init") background(0);
   button("loading...", cx, height*0.85, u*14, u*4.5);
 }
 
